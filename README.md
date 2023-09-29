0207010e9a21682402995e15381d51e12df77563
Skhmt <koshinn@gmail.com>
2023-09-28 21:45:52 -0700

    added icons

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-# Timer
-
-## TODO
-
-- Persist store in localstorage or something
-- Make PWA: see https://github.com/Skhmt/pad
+# Kill Team Clock
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,30 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
 	plugins: [
 		svelte(),
-		VitePWA({ registerType: 'autoUpdate' })
+		VitePWA({
+			includeAssets: [
+				'ktcalc.png'
+			],
+			manifest: {
+				name: 'Kill Team Clock',
+				short_name: 'KTClock',
+				description: 'Chess-clock like app for Kill Team',
+				theme_color: '#000000',
+				icons: [
+					{
+						src: 'ktclock192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'ktclock512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					},
+				]
+			},
+			registerType: 'autoUpdate'
+		})
 	],
 	base: './'
 })
