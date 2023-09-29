446482a3ece1c266488ccc2e843ee68d7bb93674
Skhmt <koshinn@gmail.com>
2023-09-28 19:25:39 -0700

    simplified full screen

--- src/lib/NoSleep.svelte
+++ src/lib/NoSleep.svelte
@@ -1,31 +1,23 @@
 <script>
     import NoSleep from 'nosleep.js';
 	import Fa from 'svelte-fa';
-	import { faMaximize, faMinimize } from '@fortawesome/free-solid-svg-icons/index.js';
+	import { faExpand } from '@fortawesome/free-solid-svg-icons/index.js';
 
 	const noSleep = new NoSleep();
-
-	let maximize = true;
 
 	const toggleNoSleep = () => {
 		if (!document.fullscreenElement) {
 			document.documentElement.requestFullscreen();
 			noSleep.enable();
-			maximize = false;
 		}
 		else if (document.exitFullscreen) {
 			document.exitFullscreen();
-			noSleep.disable();
-			maximize = true;
+			// noSleep.disable();
 		}
 		window?.navigator?.vibrate?.(100);
 	};
 </script>
 
 <button on:click={toggleNoSleep} title="Full Screen">
-	{#if maximize}
-	<Fa icon={faMaximize} />
-	{:else}
-	<Fa icon={faMinimize} />
-	{/if}
+	<Fa icon={faExpand} />
 </button>
